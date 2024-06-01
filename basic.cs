9ad1270a3d6b3643fad46d43da2842b75f98e1f3
M. Ahsan Shaikh <ahsan.sabir@yahoo.com>
2024-06-01 00:32:05 -0700

    Update basic.cs

--- basic.cs
+++ basic.cs
@@ -1,3 +1,19 @@
 string firstName;
 firstName = "Bob";
-Console.WriteLine(firstName);
+Console.WriteLine(firstName);  // Bob
+
+
+string firstName = "Bob";
+Console.WriteLine(firstName);  // Bob
+
+
+string firstName;
+Console.WriteLine(firstName) // (2,19): error CS0165: Use of unassigned local variable 'firstName'
+
+
+int firstName;
+firstName = "Bob"; // (2,9): error CS0029: Cannot implicitly convert type 'string' to 'int'
+
+
+string firstName;
+"Bob" = firstName // (2,1): error CS0131: The left-hand side of an assignment must be a variable, property or indexer
