f79e048afa4045cf029b5a0978a88a65941d6b50
M. Ahsan Shaikh <ahsan.sabir@yahoo.com>
2024-06-04 01:27:21 -0700

    Update foreach-array.cs

--- foreach-array.cs
+++ foreach-array.cs
@@ -1,1 +1,26 @@
 
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
